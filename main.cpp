17e26805dbb71abb1cd1c04d12eb627cb1997ae5
Justine Wang <justinelwang@gmail.com>
2026-05-05 14:21:27 -0700

    while loop and intro text for text adventure game

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+bool gameRunning {true};
+
+std::string intro { "The rising sun slowly brightens your bedroom, lulling you from your dreams. Reluctantly, you pull yourself away from your slumber, opening your eyes a smidgen to help reacquaint yourself with the waking world. Blinking a few times to clear the sleep matter from your eyes and let your pupils adjust, you see a basket sitting on a small stool in the corner of the room. Pulling back the bedcovers and sitting up, you stretch your arms high up into the air, accompanied by a yawn.\n\nBesides the basket on the stool and the bed you’re in, the room has three doors. Two of the doors are closed. The third is open a couple of inches.\n\nWhat would you like to do?\n\n" };
+
+int main() {
+
+    while (gameRunning) {
+        std::cout << intro;
+        gameRunning = false;
+    }
+
+    return 0;
+}
