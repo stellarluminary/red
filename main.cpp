e814d05a85f09bc88d73e1da2b26714c9699351c
Justine Wang <justinelwang@gmail.com>
2026-05-06 21:00:14 -0700

    Added options to the intro text

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ std::string intro { "The rising sun slowly brightens your bedroom, lulling you f
     "bedcovers and sitting up, you stretch your arms high up into the air, accompanied" 
     "by a yawn.\n\nBesides the basket on the stool and the bed you’re in, the room has" 
     " three doors. Two of the doors are closed. The third is open a couple of inches.\n\n"
-    "What would you like to do?\n\n" };
+    "What would you like to do (enter 1, 2, or 3)?\n\n"
+    "1: Push open the ajar door.\n"
+    "2: Open the first closed door.\n"
+    "3: Open the second closed door.\n"
+};
 
 int main() {
 
